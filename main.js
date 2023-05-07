90e73760d9c863b212e50ac45b6cd393e9a39062
Tonmoysifat <tonmoysifatmd@gmail.com>
2023-05-07 23:13:58 +0600

    6th c

--- main.js
+++ main.js
@@ -14,7 +14,8 @@ function getWeather(searchQuery) {
     return;
   }
 
-  var apiKey = process.env.API_KEY;
+  // var apiKey = process.env.API_KEY;
+  var apiKey = window.API_KEY;
 
   var url =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
