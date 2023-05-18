eee2a14498931d3c933948526548d61c130a1db9
Tonmoysifat <tonmoysifatmd@gmail.com>
2023-05-18 23:44:36 +0600

    fixed all errors

--- main.js
+++ main.js
@@ -1,47 +1,10 @@
-let catPicB = document.querySelector(".catPicB");
-catPicB.addEventListener("click", () => {
-  $("#image").append('<img class="rounded-circle" src="images/high-five.gif">');
-  $(".highFive").html("High five! You're building your first web app!");
-});
-
-$(document).ready(function () {
-  getWeather();
-});
-
-function getWeather(searchQuery) {
-  if (searchQuery === "" || searchQuery === undefined) {
-    $(".error-message").text("");
-    return;
-  }
-
-  var url =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    searchQuery +
-    "&appid=26ddbaac9e6c359a53b14b7a202b5011&units=metric";
-
-  $.ajax(url, {
-    success: function (data) {
-      console.log(data);
-      $(".temp").text(data.main.temp + "°C");
-      $(".error-message").text("");
-    },
-    error: function (error) {
-      $(".error-message").text("Can't find the city");
-      $(".city").text("");
-      $(".temp").text("");
-    },
-  });
-}
-
-let weatherBtn = document.getElementById("weatherBtn");
-weatherBtn.addEventListener("click", () => {
-  var searchQuery = $(".search").val();
-  getWeather(searchQuery);
-});
-
-import {initializeApp} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-import {getAnalytics} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-analytics.js";
-import {getAuth,signInWithRedirect,GoogleAuthProvider} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-analytics.js";
+import {
+  getAuth,
+  signInWithRedirect,
+  GoogleAuthProvider,
+} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
